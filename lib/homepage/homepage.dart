9b089e4319b4b3aaaf975d48d84bcf8d7dd9cf53
ClementPeter <clementpeter2000@gmail.com>
2024-06-06 11:53:19 +0100

    first commit

---
+++ lib/homepage/homepage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:meals_ui/homepage/widgets/app_bar.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: customAppBar(),
+    );
+  }
+}
---
+++ lib/homepage/widgets/app_bar.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+AppBar CustomAppBar() {
+  return AppBar(
+    title: const Text(
+      'Breakfast',
+      style: TextStyle(
+        color: Colors.black,
+        fontSize: 18,
+        fontWeight: FontWeight.bold,
+      ),
+    ),
+    backgroundColor: Colors.white,
+    elevation: 0.0,
+    centerTitle: true,
+    leading: GestureDetector(
+      onTap: () {},
+      child: Container(
+        margin: const EdgeInsets.all(10),
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: const Color(0xFFD8D9D9),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: SvgPicture.asset(
+          'assets/icons/Arrow - Left 2.svg',
+          height: 20,
+          width: 20,
+        ),
+      ),
+    ),
+    actions: [
+      GestureDetector(
+        child: Container(
+          width: 25,
+          margin: const EdgeInsets.all(10),
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            color: const Color(0xFFD8D9D9),
+            borderRadius: BorderRadius.circular(10),
+          ),
+          child: SvgPicture.asset(
+            'assets/icons/dots.svg',
+            height: 5,
+            width: 5,
+          ),
+        ),
+      )
+    ],
+  );
+}
